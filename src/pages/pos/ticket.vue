0a24273b94e2d51c25261dfad928b191e767d3a3
luciouribe <uribemty0602@gmail.com>
2023-03-26 14:52:40 -0600

    add(): logos

--- src/pages/pos/ticket.vue
+++ src/pages/pos/ticket.vue
@@ -1,23 +1,26 @@
 <template>
-<div class="q-pa-md text-center" style="font-size: 18px; width:100%">
- <img :src="logoIMG" height="30" width="30" /><br>
-<p>{{empresa.nombre}}</p>
-<p>{{empresa.header_ticket}}</p>
+<div class="q-pa-md text-center" style="font-size: 18px; width:110%">
+ <img src="../../assets/logo_barber-sn-fondo.png" height="70" width="70" />
+ <img src="../../assets/wendys_completo.png" height="70" width="70"/><br>
+</div>
+<div class="text-center" style="font-size: 10px;">
+    <p >C. Bosques de México 323, Los Heroes Tecamac, 55764 Ojo de Agua, Méx.</p>
+    <p>55 3763 9638</p>
     {{orden.nombre}}<br>
-   {{orden.fecha_orden}}<br>
+    {{orden.fecha_orden}}
+</div>
+<div class="q-pa-md text-center" style="font-size: 10px; width:100%">
     <div v-for="(item, index) in lineas">
         <p v-if="String(item.cantidad).concat(' ',item.nombre).slice(0, 16).length==16">
-            {{ String(item.cantidad).concat(' ',item.nombre).slice(0, 16) }} <b v-for="i in 15">-</b> ${{item.precio}}</p>
+            {{ String(item.cantidad).concat(' ',item.nombre).slice(0, 16) }} <b v-for="i in 5">-</b> ${{item.precio}}</p>
         <p v-else="">
             {{ String(item.cantidad).concat(' ',item.nombre).slice(0, 16) }}
             <b v-for="i in Number(16)-Number(String(item.cantidad).concat(' ',item.nombre).length)+15">-</b> ${{item.precio}}
         </p>
-    </div><br><br>
+    </div>
     Total: <b v-for="i in 24">-</b> ${{orden.monto_total}}<br>
     Cambio: <b v-for="i in 24">-</b> ${{orden.monto_total-orden.monto_pagado}}<br><br>
     <p style="line-height: 200%">{{empresa.footer_ticket}}</p>
-    <br><br><br><br><br><br><br><br>
-
 </div>
 <br><br>
 </template>
@@ -47,7 +50,6 @@ export default {
     mounted() {
         this.orden_cal
         var urlsplit = localStorage.getItem("auth_url").split("/api/")[0];
-        this.logoIMG =  "icons/default.png";
         sessionStorage.setItem("orden", '');
         // const myTimeout = setTimeout(window.close(), 50000);
 
