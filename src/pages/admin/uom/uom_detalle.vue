e50ce53183250716c143624896118beefee1b965
luciouribe <uribemty0602@gmail.com>
2023-04-05 10:07:55 -0600

    fix(): color de letras

--- src/pages/admin/uom/uom_detalle.vue
+++ src/pages/admin/uom/uom_detalle.vue
@@ -1,8 +1,8 @@
 <template>
   <q-page padding>
-    <div class="bg-visuel text-white rounded-borders">
+    <div class="bg-visuel text-black rounded-borders">
       <q-toolbar inset>
-        <q-breadcrumbs active-color="white" style="font-size: 15px">
+        <q-breadcrumbs active-color="black" style="font-size: 15px">
           <q-breadcrumbs-el
             icon="inventory_2"
             to="/admin/uom_producto"
@@ -140,7 +140,7 @@
                   @click="updateOn"
                   v-show="edit && setVisible('inventario.uom.create')"
                   label="Editar"
-                  color="visuel"
+                  color="red"
                   icon="mode_edit"
                   size="sm"
                 >
